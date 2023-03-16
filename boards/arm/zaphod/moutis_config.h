f1180bc6129b1cb15c7d767048f8e44138fd58d2
moutis <35399827+moutis@users.noreply.github.com>
2023-03-16 22:35:00 +0100

    ZMK_MACRO

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -8,21 +8,21 @@
 #define ___  &trans
 
 #define l_any   -1
-#define l_alpha  0
-#define l_adaptL 1
-#define l_adaptHU 2
-#define l_nav    3
-#define l_sym    4
-#define l_fn     5
-#define l_num    6
-#define l_cfg    7
+#define l_alpha 0
+#define l_akL   1
+#define l_akHU  2
+#define l_nav   3
+#define l_sym   4
+#define l_fn    5
+#define l_num   6
+#define l_cfg   7
 
 
 &mt {
     tapping-term-ms = <170>;
 };
 &sl {
-    release-after-ms = <200>;
+    release-after-ms = <170>;
 };
 
 #define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
