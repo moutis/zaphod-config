2dd9407288d3039af9bfa7af4cdcb5e4d2eb17d4
moutis <35399827+moutis@users.noreply.github.com>
2023-03-16 21:52:09 +0100

    l_adaptHU

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -7,15 +7,18 @@
 
 #define ___  &trans
 
-
 #define l_any   -1
-#define l_alpha  0
-#define l_adaptL 1
-#define l_nav    2
-#define l_sym    3
-#define l_fn     4
-#define l_num    5
-#define l_cfg    6
+enum my_layers {
+    l_alpha,
+    l_adaptL,
+    l_adaptHU,
+    l_nav,
+    l_sym,
+    l_fn,
+    l_num,
+    l_cfg
+};
+
 
 
 &mt {
