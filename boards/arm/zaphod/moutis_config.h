1164e5f8abd6aef1d6e4fa5352dacde836597af1
moutis <35399827+moutis@users.noreply.github.com>
2023-03-17 11:56:45 +0100

    l_akEO

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -11,11 +11,12 @@
 #define l_alpha 0
 #define l_akL   1
 #define l_akHU  2
-#define l_nav   3
-#define l_sym   4
-#define l_fn    5
-#define l_num   6
-#define l_cfg   7
+#define l_akEO  3
+#define l_nav   4
+#define l_sym   5
+#define l_fn    6
+#define l_num   7
+#define l_cfg   8
 
 #define my-tapping-term 170
 
@@ -23,7 +24,7 @@
     tapping-term-ms = <170>;
 };
 &sl {
-    release-after-ms = <500>;
+    release-after-ms = <200>;
 };
 
 #define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
