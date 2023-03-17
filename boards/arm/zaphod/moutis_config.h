4c6ee653a098bdafb63fa3dd63bc08c42c709832
moutis <35399827+moutis@users.noreply.github.com>
2023-03-17 13:26:13 +0100

    my_tapping_term

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -18,17 +18,18 @@
 #define l_num   7
 #define l_cfg   8
 
-#define my-tapping-term 170
+#define my_tapping_term 170
+#define my_quick_tapping_term 170
 
 #define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24  // left-hand keys
 #define KEYS_R 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29  // right-hand keys
 #define THUMBS 30 31 32 33                              // thumb keys
 
 &mt {
-    tapping-term-ms = <170>;
+    tapping-term-ms = <my_tapping_term>;
 };
 &sl {
-    release-after-ms = <170>;
+    release-after-ms = <my_tapping_term>;
 };
 
 #define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
