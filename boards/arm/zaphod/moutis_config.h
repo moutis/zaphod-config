4b29a70e269b4b40eff35188913c79d32c4f581b
moutis <35399827+moutis@users.noreply.github.com>
2024-06-30 16:20:59 -0600

    l_Caps layer for commacap

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -85,12 +85,12 @@
 #define l_akO   17
 #define l_akI   18
 #define l_akDot 19
-#define l_sym   20
-#define l_num   21
-#define l_fun   22
-#define l_nav   23
-#define l_cfg   24
-#define l_CCaps	25
+#define l_CCaps	20
+#define l_sym   21
+#define l_num   22
+#define l_fun   23
+#define l_nav   21
+#define l_cfg   25
 
 #define my_tapping_term 170
 #define my_quick_tapping_term 112
