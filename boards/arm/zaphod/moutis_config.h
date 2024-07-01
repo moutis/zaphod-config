c565ec58ff59cf63a82f1c7764a65b0087b03af6
moutis <35399827+moutis@users.noreply.github.com>
2024-06-30 19:32:49 -0600

    #ifdef COMMA_CAP

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -6,6 +6,16 @@
 //
 
 
+#define H_DIGRAPH_COMBOS //  (th, ch, sh, wh, gh, ph)
+#define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
+#define COMMA_CAP 	// , before alpha deletes , and capitalizes alpha
+
+#define my_tapping_term 170
+#define my_quick_tapping_term 112
+#define my_ak_delay 40
+
+
+
 /*    KEY POSITION Numbers for Zaphod
  ╭────────────────────╮ ╭────────────────────╮
  │  0   1   2   3   4 │ │  5   6   7   8   9 │
@@ -85,16 +95,14 @@
 #define l_akO   17
 #define l_akI   18
 #define l_akDot 19
-#define l_CCaps	20
-#define l_sym   21
-#define l_num   22
-#define l_fun   23
-#define l_nav   24
-#define l_cfg   25
-
-#define my_tapping_term 170
-#define my_quick_tapping_term 112
-#define my_ak_delay 40
+#define l_sym   20
+#define l_num   21
+#define l_fun   22
+#define l_nav   23
+#define l_cfg   24
+#ifdef COMMA_CAP
+#define l_CCaps	25
+#endif
 
 &mt {
     tapping-term-ms = <my_tapping_term>;
@@ -107,4 +115,3 @@
 };
 
 
-#define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
