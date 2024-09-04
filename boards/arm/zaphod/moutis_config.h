75acda795b0391a672fb6256740236bb6a0dcd25
moutis <35399827+moutis@users.noreply.github.com>
2024-09-04 16:23:03 -0400

    sync HD-vx with other boards

--- boards/arm/zaphod/moutis_config.h
+++ boards/arm/zaphod/moutis_config.h
@@ -8,12 +8,11 @@
 
 #define H_DIGRAPH_COMBOS //  (th, ch, sh, wh, gh, ph)
 #define EN_PRONOUN_COMBOS //  (I, I'm, I've I'd I'll etc)
-#define COMMA_CAP 	// , before alpha deletes , and capitalizes alpha
+//#define COMMA_CAP 	// , before alpha deletes , and capitalizes alpha
 
 #define my_tapping_term 170
 #define my_quick_tapping_term 112
 #define my_ak_delay 40
-
 
 
 /*    KEY POSITION Numbers for Zaphod
@@ -22,14 +21,17 @@
  │ 10  11  12  13  14 │ │ 15  16  17  18  19 |
  | 20  21  22  23  24 │ │ 25  26  27  28  29 |
  ╰───────────╮ 30  31 | | 32  33  ╭──────────╯
-             ╰────────╯ ╰─────────╯   */
+             ╰────────╯ ╰─────────╯   
+ */
 /*    KEY POSITION Names
 ╭─────────────────────╮ ╭─────────────────────╮
 │ LT4 LT3 LT2 LT1 LT0 │ │ RT0 RT1 RT2 RT3 RT4 │
 │ LM4 LM3 LM2 LM1 LM0 │ │ RM0 RM1 RM2 RM3 RM4 │
 │ LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 │
 ╰───────────╮ LH2 LH1 | | RH1 RH2 ╭───────────╯
-            ╰─────────╯ ╰─────────╯   */
+            ╰─────────╯ ╰─────────╯   
+ */
+
 #define LT0 4
 #define LT1 3
 #define LT2 2
@@ -119,5 +121,3 @@
 &caps_word {
     continue-list = <UNDERSCORE MINUS BSPC DEL LEFT RIGHT>;
 };
-
-
